06f7c8badd9b169b9f32d228558f99b3ce4d61b1
Mehdi Meknaci <mehdi.meknaci@epitech.eu>
2020-03-04 11:51:50 -0500

    Moove testing scripts

--- dl_files/container.sh
+++ dl_files/container.sh
@@ -18,11 +18,11 @@ function container()
     sudo chmod 777 /var/run/docker.sock
     echo "starting update and download..."
     docker pull epitechcontent/epitest-docker
-    docker run --name fedora -i -d -v ~/.ssh:/root/.ssh -v $HOME:/root/current -w /root epitechcontent/epitest-docker:latest
     echo "moving docker starting scripts.."
     scripts
     sleep 0.5
     echo "Done !"
+    docker run --name fedora -i -d -v ~/.ssh:/root/.ssh -v $HOME:/root/current -w /root epitechcontent/epitest-docker:latest
     echo ""
     echo "Epitech Docker Packages are installed !"
 }
